2ba584d7a367f046b90430e8d917839cfdfad592
gabrielott <gabrielott@icloud.com>
2020-03-28 22:35:27 -0300

    bspwm rules and .xprofile fix

---
+++ .config/bspwm/rules.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+id="$1"
+class="$2"
+instance="$3"
+
+case "$class" in
+	'Hydrus Client')
+		case "$(xprop -id "$id" WM_NAME | cut -d'"' -f2)" in
+			'hydrus client media viewer') echo 'state=floating' ;;
+			'hydrus main') echo 'locked=on' ;;
+			*) echo 'focus=on desktop=DP-4:^3' ;;
+		esac ;;
+	'Anki')
+		case "$(xprop -id "$id" WM_NAME | cut -d'"' -f2)" in
+			'ユーザー 1 - Anki') echo 'desktop=DP-4:^2 locked=on' ;;
+			'Dictionary') echo 'desktop=DVI-D-0:^1 locked=on' ;;
+		esac ;;
+	'qBittorrent')
+		case "$(xprop -id "$id" WM_NAME | cut -d'"' -f2)" in
+			'qBittorrent v'*) echo 'desktop=DVI-D-0:^2 locked=on' ;;
+		esac ;;
+esac
