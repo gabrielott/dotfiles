f9c56bab20791518e1fc6d0ce9ac96d086e331f2
gabrielott <gabrielott@icloud.com>
2022-06-11 13:00:27 -0300

    Refactor setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,14 @@
 #!/bin/sh
+
 # Downloads some config files and sets my keyboard layout.
 
-curl "https://raw.githubusercontent.com/gabrielott/dotfiles/master/.vimrc" > "$HOME"/.vimrc
-curl "https://raw.githubusercontent.com/gabrielott/dotfiles/master/.inputrc" > "$HOME"/.inputrc
+curl -q 'https://raw.githubusercontent.com/gabrielott/dotfiles/master/.vimrc' -o ~/.vimrc
+curl -q 'https://raw.githubusercontent.com/gabrielott/dotfiles/master/.inputrc' -o ~/.inputrc
 
-lsusb | cut -d ' ' -f 7- | grep 'Holtek Semiconductor, Inc. USB-HID Keyboard' > /dev/null && setxkbmap 'us' || setxkbmap 'us(colemak)'
+if lsusb | fgrep -q 'Holtek Semiconductor, Inc. USB-HID Keyboard'; then
+	echo 'Keyboard detected, setting xkb layout to US.'
+	setxkbmap us
+else
+	echo 'Keyboard not detected, setting xkb layout to Colemak and swapping ctrl with caps lock.'
+	setxkbmap us colemak ctrl:swapcaps
+fi
